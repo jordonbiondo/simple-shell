d985e4c5c1449a3acbc091b2e6ea04b67c5239ac
jordonbiondo <biondoj@mail.gvsu.edu>
2013-09-10 20:40:13 -0400

    removed unused includes

--- src/simple-shell.c
+++ src/simple-shell.c
@@ -30,11 +30,10 @@
 #include <sys/wait.h>
 #include <sys/time.h>
 #include <unistd.h>
-#include <stdlib.h>
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
-#include <time.h>
+
 
 #include "simple-shell.h"
 #include "ss-strings.h"
